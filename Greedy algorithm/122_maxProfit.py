f7f4c0f927e825510d5ce588b1195f5ec5c94fc5
Hansen06 <haishenmuc@163.com>
2023-02-28 23:27:28 +0800

    122

---
+++ Greedy algorithm/122_maxProfit.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*- #
+# Name        : 122_maxProfit.py
+# Author      : shenchenlove 
+# Time        : 2023/2/28 23:16
+# Description : 122 买卖股票的最佳时机 II
+
+from typing import *
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        '''
+
+        :param prices:
+        :return:
+        '''
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
