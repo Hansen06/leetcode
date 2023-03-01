77b94556052e9a227aad12bd4270195f6e917ec1
‘Hansen06’ <haishenmuc@163.com>
2023-03-01 10:46:31 +0800

    466 665

---
+++ Greedy algorithm/466_reconstructQueue.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*- #
+# Name        : 466_reconstructQueue.py
+# Author      : haishen yao
+# Time        : 2023/3/1 10:44
+# Description : 466 根据身高重建队列
+
+from typing import *
+
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
---
+++ Greedy algorithm/665_checkPossibility.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*- #
+# Name        : 665_checkPossibility.py
+# Author      : haishen yao
+# Time        : 2023/3/1 10:45
+# Description : 665 非递减数列
+
+from typing import *
+
+class Solution:
+    def checkPossibility(self, nums: List[int]) -> bool:
+
+
+
+if __name__ == '__main__':
+    solution = Solution()
