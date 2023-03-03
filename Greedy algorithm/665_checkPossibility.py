3b68945e220d3da69f71de1cb8a866e111a78b0a
‘Hansen06’ <haishenmuc@163.com>
2023-03-03 18:31:38 +0800

    665

--- Greedy algorithm/665_checkPossibility.py
+++ Greedy algorithm/665_checkPossibility.py
@@ -8,8 +8,24 @@ from typing import *
 
 class Solution:
     def checkPossibility(self, nums: List[int]) -> bool:
+        '''
+
+        :param nums:
+        :return:
+        '''
+        change = 0
+        le = len(nums)
+        for i in range(1, le):
+            if nums[i] < nums[i-1]:
+                if (i <= le-2 and nums[i+1]-nums[i-1] >= 1):
+                    change += 1
+        if change > 1:
+            return False
+        return True
 
 
 
 if __name__ == '__main__':
     solution = Solution()
+    nums = [4, 2, 3]
+    print(solution.checkPossibility(nums))
