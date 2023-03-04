f5480802e84512467e0e8faef54d853144bb2adb
Hansen06 <haishenmuc@163.com>
2023-03-04 21:15:11 +0800

    406

---
+++ Greedy algorithm/406_reconstructQueue.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*- #
+# Name        : 406_reconstructQueue.py
+# Author      : shenchenlove 
+# Time        : 2023/3/4 21:13
+# Description : 406 根据身高重建队列
+
+from typing import *
+
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        '''
+
+        :param people:
+        :return:
+        '''
+
+
+
+
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
