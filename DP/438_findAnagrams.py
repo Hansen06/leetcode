83dadbc8fd5722930092ccc8b00b00cf4a7ccffe
Hansen06 <haishenmuc@163.com>
2023-05-25 18:30:49 +0800

    438

--- DP/438_findAnagrams.py
+++ DP/438_findAnagrams.py
@@ -5,42 +5,43 @@
 # Description : 438. 找到字符串中所有字母异位词
 
 from typing import *
+
+
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
+        '''
+        滑动窗口+桶思想+ascii码
+        :param s:
+        :param p:
+        :return:
+        '''
         if len(s) < len(p):
             return []
         k = len(p)
-        p2n = {}
-        for i in p:
-            if p2n.get(i) is not None:
-                p2n[i] += 1
-            else:
-                p2n[i] = 1
-        slow = 0
-        fast = k
         res = []
-        flag = 0
-        while fast <= len(s):
-            k2n = {}
-            for i in s[slow:fast]:
-                if k2n.get(i) is not None:
-                    k2n[i] += 1
-                else:
-                    k2n[i] = 1
-            for k, v in p2n.items():
-                if k2n.get(k) != v:
-                    flag = 1
-                    break
+        s_count = [0] * 26
+        p_count = [0] * 26
+        for i in range(k):
+            s_count[ord(s[i]) - 97] += 1
+            p_count[ord(p[i]) - 97] += 1
 
-            if flag != 1:
+        if s_count == p_count:
+            res.append(0)
+
+        slow = 1
+        fast = k
+        while fast < len(s):
+            s_count[ord(s[slow - 1]) - 97] -= 1
+            s_count[ord(s[fast]) - 97] += 1
+            if s_count == p_count:
                 res.append(slow)
             slow += 1
             fast += 1
-            flag = 0
         return res
+
 
 if __name__ == '__main__':
     solution = Solution()
     s = 'aaa'
-    b = 'aa'
+    p = 'aa'
     print(solution.findAnagrams(s, p))
